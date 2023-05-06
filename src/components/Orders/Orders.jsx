70ccf794704207e5bbf85ccb4760bc7a280ace2a
Sharminaktermetu <sharminacc1@gmail.com>
2023-05-06 23:28:52 +0600

    final loacal storage and ul

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -4,16 +4,19 @@ const Orders = ({cart}) => {
   
     let total =0;
     let totalShipping= 0;
+    let quantity =0;
     for(const product of cart){
-        total=total+product.price;
-        totalShipping=totalShipping+product.shipping
+        product.quantity= product.quantity||1
+        total=total+product.price*product.quantity;
+        totalShipping=totalShipping+product.shipping;
+        quantity =quantity+product.quantity
     }
     const tax = total*7/100;
     const grand =total+totalShipping+tax;
     return (
         <div className="orders">
         <h3>Order summery</h3>
-        <strong>Selected Items: {cart.length}</strong>
+        <strong>Selected Items: {quantity}</strong>
         <p>Total price: ${total}</p>
         <p>Total shipping:${totalShipping}</p>
         <p>Tax:${tax}</p>
